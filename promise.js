39c69561ca7c83c21989a42b1a06ebb92e5af3a7
Julian Connor <julian.connor@venmo.com>
2013-05-19 17:28:20 -0400

    Cleans up old unused variables.

--- promise.js
+++ promise.js
@@ -5,11 +5,8 @@ var Events = require('./events');
       S_RESOLVED  = "resolved",
       S_REJECTED  = "rejected";
 
-  var counter = 0;
-
   var Promise = function(fn) {
     this.state = S_PENDING;
-
     this.reason = null;
 
     if ( fn ) fn(this.resolve, this.reject);
@@ -34,39 +31,29 @@ var Events = require('./events');
   };
 
   Promise.prototype.resolve = function(val) {
-    var carriedVal;
-
     if ( this.state === S_PENDING ) {
       this.state = S_RESOLVED;
-      if ( this.onFulfilled && !this.onFulfilled.called ) {
+
+      if ( this.onFulfilled && !this.onFulfilled.called )
         this.trigger('resolved', this.onFulfilled(val));
-      }
     }
 
     return this;
   };
 
   Promise.prototype.reject = function(reason) {
-    var fn;
-
     if ( this.state === S_PENDING ) {
       this.state = S_REJECTED;
       this.reason = reason;
 
-      if ( this.onRejected && !this.onRejected.called ) {
+      if ( this.onRejected && !this.onRejected.called )
         this.trigger('reject', this.onRejected(reason));
-      }
     }
 
     return this;
   };
 
   Events.extend(Promise.prototype);
-
-  function each(lst, fn) {
-    for ( var i = 0; i < lst.length; i++ )
-      fn(lst[i], i);
-  }
 
   if ( typeof exports !== "undefined" )
     exports = module.exports = Promise;
